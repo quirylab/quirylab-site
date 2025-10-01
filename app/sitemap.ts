a5c23f8a7b3e03fc322970a78cf9408ca93c9f7f
ayane0857 <tailangrongyi@gmail.com>
2025-10-01 14:06:14 +0000

    サイトマップとロボットを作った

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,37 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://acme.com",
+      url: "https://quirylab.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://acme.com/about",
+      url: "https://quirylab.com/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
+      priority: 0.9,
     },
     {
-      url: "https://acme.com/blog",
+      url: "https://quirylab.com/service",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://quirylab.com/articles-of-incorporation",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://quirylab.com/terms-of-service",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://quirylab.com/privacy-policy",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
